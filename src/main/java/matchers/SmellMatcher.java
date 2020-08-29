7e0d02f5533dab6cd131310447be7bf2f2733144
gvma <Guiga30.123>
2020-08-28 21:25:44 -0300

    Updated sniff method to work with reflection

--- src/main/java/matchers/SmellMatcher.java
+++ src/main/java/matchers/SmellMatcher.java
@@ -2,8 +2,6 @@ package matchers;
 
 import utils.TestClass;
 
-import java.util.concurrent.Callable;
-
 public abstract class SmellMatcher {
-    protected abstract Callable<?> match(TestClass testClass);
+    protected abstract void match(TestClass testClass);
 }
