87320aab8d6b8b49fa4aab0dccfaf08d36cd19d0
Guilherme Amaral <40870111+gvma@users.noreply.github.com>
2021-10-14 10:16:28 -0300

    Removed github clone

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import github.GithubRepositoryClone;
 import utils.JUnit5Utilization;
 
 import java.io.File;
@@ -7,22 +6,18 @@ import java.util.Objects;
 public class Main {
     public synchronized static void main(String[] args) throws Exception {
         if (args.length > 0) {
-            if (args[0].equals("-githubClone")) {
-                new GithubRepositoryClone(args[1], args[2], args[3], args[4], args[5]);
-            } else {
-                if (args[0].equals("-multipleProjects")) {
-                    File rootDir = new File(args[1]);
-                    for (File file : Objects.requireNonNull(rootDir.listFiles())) {
-                        if (file.isDirectory()) {
-                            JUnit5Utilization jUnit5Utilization = new JUnit5Utilization(args[1] + '\\' + file.getName());
-                            jUnit5Utilization.findJUnit5Imports();
-                        }
+            if (args[0].equals("-multipleProjects")) {
+                File rootDir = new File(args[1]);
+                for (File file : Objects.requireNonNull(rootDir.listFiles())) {
+                    if (file.isDirectory()) {
+                        JUnit5Utilization jUnit5Utilization = new JUnit5Utilization(args[1] + '\\' + file.getName());
+                        jUnit5Utilization.findJUnit5Imports();
                     }
-                } else {
-                    JUnit5Utilization jUnit5Utilization = new JUnit5Utilization(args[0]);
-                    jUnit5Utilization.findJUnit5Imports();
                 }
-            }
+            } else {
+                JUnit5Utilization jUnit5Utilization = new JUnit5Utilization(args[0]);
+                jUnit5Utilization.findJUnit5Imports();
+            }  
         } else {
             throw new IllegalArgumentException("You must provide a correct file path with a java project!");
         }
