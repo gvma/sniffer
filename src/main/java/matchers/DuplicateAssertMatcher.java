f07540e241eabcaf6931d701563ff97b03adc90e
gacl97 <gacl@ic.ufal.br>
2022-09-22 23:36:39 -0300

    Duplicate Assert and Exception Handling Smell Matchers

--- src/main/java/matchers/DuplicateAssertMatcher.java
+++ src/main/java/matchers/DuplicateAssertMatcher.java
@@ -1,70 +1,56 @@
-//package matchers;
-//
-//import com.github.javaparser.ast.Node;
-//import com.github.javaparser.ast.visitor.TreeVisitor;
-//import utils.OutputWriter;
-//import utils.TestClass;
-//import utils.TestMethod;
-//
-//import java.util.HashMap;
-//import java.util.LinkedList;
-//import java.util.List;
-//import java.util.Map;
-//import java.util.logging.Logger;
-//
-//public class DuplicateAssertMatcher extends SmellMatcher {
-//    @Override
-//    protected void match(TestClass testClass) {
-//        for (TestMethod testMethod : testClass.getTestMethods()) {
-//            for (Node node : testMethod.getMethodDeclaration().getChildNodes()) {
-//                List<Integer> lines = new LinkedList<>();
-//                Map<String, List<Integer>> allAsserts = new HashMap<>();
-//                getAllAsserts(node.getChildNodes(), allAsserts);
-//                for (Map.Entry<String, List<Integer>> entry : allAsserts.entrySet()) {
-//                    if (entry.getValue().size() > 1) {
-//                        for (Integer line : entry.getValue()) {
-//                            if (!lines.contains(line)) {
-//                                lines.add(line);
-//                            }
-//                        }
-//                    }
-//                }
-//                if (!lines.isEmpty()) {
-//                    write(testMethod.getTestFilePath(), "Duplicate Assert", testMethod.getMethodDeclaration().getNameAsString(), lines.toString());
-//                }
-//            }
-//        }
-//    }
-//
-//    @Override
-//    public void write(String filePath, String testSmell, String name, String lines) {
-//        OutputWriter.getInstance().write(filePath, testSmell, name, lines);
-//        Logger.getLogger(AssertionRouletteMatcher.class.getName()).info("Found duplicate assert in method \"" + name + "\" in lines " + lines);
-//    }
-//
-//    private void getAllAsserts(List<Node> nodeList, Map<String, List<Integer>> allAsserts) {
-//        List<Node> nodes = new LinkedList<>();
-//        for (Node node : nodeList) {
-//            new TreeVisitor() {
-//                @Override
-//                public void process(Node node) {
-//                    if (node.getRange().isPresent()) {
-//                        if (node.getMetaModel().getTypeName().equals("ExpressionStmt")) {
-//                            if (node.toString().trim().startsWith("assert") || node.toString().trim().startsWith("Assert")) {
-//                                if (node.getRange().isPresent()) {
-//                                    allAsserts.computeIfAbsent(node.toString().trim(), k -> new LinkedList<>());
-//                                    List<Integer> lines = allAsserts.get(node.toString());
-//                                    if (!lines.contains(node.getRange().get().begin.line)) {
-//                                        nodes.add(node);
-//                                        lines.add(node.getRange().get().begin.line);
-//                                    }
-//                                    allAsserts.put(node.toString().trim(), lines);
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//            }.visitPreOrder(node);
-//        }
-//    }
-//}
+package matchers;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.traversal.DocumentTraversal;
+import org.w3c.dom.traversal.NodeFilter;
+import org.w3c.dom.traversal.TreeWalker;
+import utils.OutputWriter;
+import utils.TestClass;
+import utils.TestMethod;
+
+import java.util.*;
+import java.util.logging.Logger;
+
+public class DuplicateAssertMatcher extends SmellMatcher {
+
+  @Override
+  protected void match(TestClass testClass) {
+    for (TestMethod testMethod : testClass.getTestMethods()) {
+      NodeList childrenMethods = testMethod.getMethodDeclaration().getChildNodes();
+      Map<String, Integer> allAsserts = new HashMap<>();
+      getAllAsserts(childrenMethods, allAsserts);
+      for (Map.Entry<String, Integer> entry : allAsserts.entrySet()) {
+        if (entry.getValue() > 1) {
+          write(testMethod.getTestFilePath(), "Duplicate Assert", testMethod.getMethodName(), new LinkedList<>().toString());
+        }
+      }
+    }
+  }
+
+  @Override
+  public void write(String filePath, String testSmell, String name, String lines) {
+    OutputWriter.getInstance().write(filePath, testSmell, name, lines);
+    Logger.getLogger(AssertionRouletteMatcher.class.getName()).info("Found duplicate assert in method \"" + name + "\" in lines " + lines);
+  }
+
+  private void getAllAsserts(NodeList nodeList, Map<String, Integer> allAsserts) {
+    for (int i = 0; i < nodeList.getLength(); ++i) {
+      Node root = nodeList.item(i);
+      DocumentTraversal traversal = (DocumentTraversal) nodeList.item(i).getOwnerDocument();
+      TreeWalker iterator = traversal.createTreeWalker(root, NodeFilter.SHOW_ALL, null, false);
+      Node node = null;
+      while ((node = iterator.nextNode())!=null){
+        String textContent = node.getTextContent().trim();
+        if (node.getNodeName().equals("expr") && (textContent.startsWith("EXPECT_") || textContent.startsWith("ASSERT_"))) {
+//          System.out.println("Node: " + node.getNodeName() + " content: " + textContent);
+          if (allAsserts.containsKey(node.getTextContent())) {
+            allAsserts.put(node.getTextContent(), allAsserts.get(node.getTextContent()) + 1);
+          } else {
+            allAsserts.put(node.getTextContent(), 1);
+          }
+        }
+      }
+    }
+  }
+}
--- src/main/java/matchers/ExceptionHandlingMatcher.java
+++ src/main/java/matchers/ExceptionHandlingMatcher.java
@@ -1,78 +1,78 @@
-//package matchers;
-//
-//import com.github.javaparser.ast.Node;
-//import com.github.javaparser.ast.visitor.TreeVisitor;
-//import utils.OutputWriter;
-//import utils.TestClass;
-//import utils.TestMethod;
-//
-//import java.util.List;
-//import java.util.logging.Logger;
-//
-//public class ExceptionHandlingMatcher extends SmellMatcher {
-//
-//    @Override
-//    protected void match(TestClass testClass) {
-//        for (TestMethod testMethod : testClass.getTestMethods()) {
-//            for (Node node : testMethod.getMethodDeclaration().getChildNodes()) {
-//                if (matchExceptionHandling(node.getChildNodes())) {
-//                    write(testMethod.getTestFilePath(), "Exception Handling", testMethod.getMethodDeclaration().getNameAsString(), Integer.toString(testMethod.getBeginLine()));
-//                }
-//            }
-//        }
-//    }
-//
-//    @Override
-//    public void write(String filePath, String testSmell, String name, String lines) {
-//        OutputWriter.getInstance().write(filePath, testSmell, name, lines);
-//        Logger.getLogger(AssertionRouletteMatcher.class.getName()).info("Found Exception Handling in method \"" + name + "\" in line " + lines);
-//    }
-//
-//    private boolean matchExceptionHandling(List<Node> nodeList) {
-//        boolean[] isExceptionHandling = {false};
-//        for (Node node : nodeList) {
-//            if (isExceptionHandling[0]) {
-//                return true;
-//            }
-//            new TreeVisitor() {
-//                @Override
-//                public void process(Node node) {
-//                    if (node.getMetaModel().getTypeName().equals("TryStmt")) {
-//                        for (Node tryBlockChild : node.getChildNodes()) {
-//                            if (tryBlockChild.getMetaModel().getTypeName().equals("BlockStmt")) {
-//                                for (Node blockStmtChild : tryBlockChild.getChildNodes()) {
-//                                    if (blockStmtChild.getMetaModel().getTypeName().equals("ExpressionStmt")) {
-//                                        if (blockStmtChild.toString().trim().startsWith("Assert")
-//                                                || blockStmtChild.toString().trim().startsWith("assert")
-//                                                || blockStmtChild.toString().trim().endsWith("Assert")
-//                                                || blockStmtChild.toString().trim().endsWith("assert")
-//                                                || blockStmtChild.toString().trim().startsWith("fail")
-//                                                || blockStmtChild.toString().trim().endsWith("fail")) {
-//                                            isExceptionHandling[0] = true;
-//
-//                                        }
-//                                    }
-//                                }
-//                            } else if (tryBlockChild.getMetaModel().getTypeName().equals("CatchClause")) {
-//                                for (Node catchClauseChild : tryBlockChild.getChildNodes()) {
-//                                    if (catchClauseChild.getMetaModel().getTypeName().equals("BlockStmt")) {
-//                                        for (Node blockStmtChild : catchClauseChild.getChildNodes()) {
-//                                            if (blockStmtChild.getMetaModel().getTypeName().equals("ExpressionStmt")) {
-//                                                if (blockStmtChild.toString().trim().startsWith("Assert")
-//                                                        || blockStmtChild.toString().trim().startsWith("assert")
-//                                                        || blockStmtChild.toString().trim().startsWith("fail")) {
-//                                                    isExceptionHandling[0] = true;
-//                                                }
-//                                            }
-//                                        }
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//            }.visitPreOrder(node);
-//        }
-//        return isExceptionHandling[0];
-//    }
-//}
+package matchers;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.traversal.DocumentTraversal;
+import org.w3c.dom.traversal.NodeFilter;
+import org.w3c.dom.traversal.TreeWalker;
+import utils.OutputWriter;
+import utils.TestClass;
+import utils.TestMethod;
+
+import java.util.LinkedList;
+import java.util.logging.Logger;
+
+public class ExceptionHandlingMatcher extends SmellMatcher {
+
+  boolean hasExceptionHandlingSmell = false;
+
+  @Override
+  protected void match(TestClass testClass) {
+    for (TestMethod testMethod : testClass.getTestMethods()) {
+      Node root = testMethod.getMethodDeclaration();
+      System.out.println(testMethod.getMethodDeclaration().getTextContent());
+      matchExceptionHandlingRecursive(root, false);
+      if (hasExceptionHandlingSmell) {
+        write(testMethod.getTestFilePath(), "Exception Handling", testMethod.getMethodName(), new LinkedList<>().toString());
+      }
+      this.hasExceptionHandlingSmell = false;
+    }
+  }
+
+  @Override
+  public void write(String filePath, String testSmell, String name, String lines) {
+    OutputWriter.getInstance().write(filePath, testSmell, name, lines);
+    Logger.getLogger(AssertionRouletteMatcher.class.getName()).info("Found Exception Handling in method \"" + name + "\" in line " + lines);
+  }
+
+  // Abordagem recursiva
+  private void matchExceptionHandlingRecursive(Node root, boolean isInsideTryCatch) {
+    NodeList rootChildren = root.getChildNodes();
+    Node rootChild = null;
+    for(int i = 0; i < rootChildren.getLength(); ++i) {
+      rootChild = rootChildren.item(i);
+//      System.out.println("Node: " + rootChild.getNodeName() + " content: " + rootChild.getTextContent());
+      if(rootChild.getNodeName().equals("try") || rootChild.getNodeName().equals("catch")) {
+        matchExceptionHandlingRecursive(rootChild, true);
+      }
+      String textContent = rootChild.getTextContent().trim();
+      if(isInsideTryCatch && rootChild.getNodeName().equals("expr") && (textContent.startsWith("EXPECT_") || textContent.startsWith("FAIL") || textContent.startsWith("ASSERT_"))) {
+        this.hasExceptionHandlingSmell = true;
+      }
+      matchExceptionHandlingRecursive(rootChild, isInsideTryCatch);
+    }
+  }
+  // Segunda abordagem
+  private boolean matchExceptionHandling(NodeList nodeList) {
+    for(int j = 0; j < nodeList.getLength(); ++j) {
+      Node childBlockContent = nodeList.item(j);
+      System.out.println("Node: " + childBlockContent.getNodeName() + " content: " + childBlockContent.getTextContent());
+      if (childBlockContent.getNodeName().equals("try") || childBlockContent.getNodeName().equals("catch")) {
+        NodeList childrenTryContent = childBlockContent.getChildNodes();
+        for (int k = 0; k < childrenTryContent.getLength(); ++k) {
+          Node childTryContent = childrenTryContent.item(k);
+          DocumentTraversal traversal = (DocumentTraversal) childTryContent.getOwnerDocument();
+          TreeWalker iterator = traversal.createTreeWalker(childTryContent, NodeFilter.SHOW_ALL, null, false);
+          Node node = null;
+          while ((node = iterator.nextNode()) != null) {
+            String textContent = node.getTextContent().trim();
+            if (node.getNodeName().equals("expr") && (textContent.startsWith("EXPECT_") || textContent.startsWith("FAIL_") || textContent.startsWith("ASSERT_"))) {
+              return true;
+            }
+          }
+        }
+      }
+    }
+    return false;
+  }
+}
